714ec9f6028b638b3917a13d0e9d0778beb5cb99
koarakko5555 <hosayaan.1016@gmail.com>
2024-10-20 03:40:30 +0900

    devise関連のメッセージを日本語化

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
 describe User do
-  let(:nickname){ 'テスト太郎' }
-  let(:email){ 'test@example.com' }
-  let(:password){ '12345678' }
-  let(:user){ User.new(nickname: nickname, email: email, password: password, password_confirmation: password)}
+  let(:nickname) { 'テスト太郎' }
+  let(:email) { 'test@example.com' }
+  let(:user) { User.new(nickname: nickname, email: email, password: password, password_confirmation: password) } # 変数に格納
 
   describe '.first' do
     before do
@@ -20,17 +19,18 @@ describe User do
   end
 
   describe 'validation' do
+    let(:password) { '12345678' }
 
     describe 'nickname属性' do
-
       describe '文字数制限の検証' do
         context 'nicknameが20文字以下の場合' do
-          let(:nickname){ 'あいうえおかきくけこさしすせそたちつてと' }
+          let(:nickname) { 'あいうえおかきくけこさしすせそたちつてと' } # 20文字
 
           it 'User オブジェクトは有効である' do
             expect(user.valid?).to be(true)
           end
         end
+
         context 'nicknameが20文字を超える場合' do
           let(:nickname) { 'あいうえおかきくけこさしすせそたちつてとな' } # 21文字
 
@@ -38,18 +38,18 @@ describe User do
             user.valid?
 
             expect(user.valid?).to be(false)
-            expect(user.errors[:nickname]).to include('is too long (maximum is 20 characters)')
+            expect(user.errors[:nickname]).to include('は20文字以下に設定して下さい。')
           end
         end
       end
 
-      describe 'nickname存在性の検証' do
-        context 'nicknameが空欄の存在' do
-          let(:nickname){ '' }
+      describe '存在性の検証' do
+        context 'nicknameが空欄の場合' do
+          let(:nickname) { '' }
 
           it 'User オブジェクトは無効である' do
             expect(user.valid?).to be(false)
-            expect(user.errors[:nickname]).to include("can't be blank")
+            expect(user.errors[:nickname]).to include("が入力されていません。")
           end
         end
       end
