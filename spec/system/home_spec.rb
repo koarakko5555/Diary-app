0c9f3ecba140b9a8f27f1c0973af62544ccd0bd5
koarakko5555 <hosayaan.1016@gmail.com>
2024-10-29 03:55:01 +0900

    学習ログ投稿機能を作成

--- spec/system/home_spec.rb
+++ spec/system/home_spec.rb
@@ -32,6 +32,14 @@ RSpec.describe 'Home', type: :system do
       it 'ログアウトリンクは表示しない' do
         expect(page).not_to have_content('ログアウト')
       end
+
+      it 'ログ投稿リンクを表示しない' do # 追加
+        expect(page).not_to have_link('ログ投稿', href: '/posts/new')
+      end
+
+      it 'ログアウトリンクは表示しない' do
+        expect(page).not_to have_content('ログアウト')
+      end
     end
 
     context 'ログインしている場合' do
@@ -59,6 +67,14 @@ RSpec.describe 'Home', type: :system do
         expect(page).to have_link('ユーザー登録', href: '/users/sign_up')
         expect(page).to have_link('ログイン', href: '/users/sign_in')
         expect(page).not_to have_button('ログアウト')
+      end
+
+      it 'ログ投稿リンクを表示する' do # 追加
+        expect(page).to have_link('ログ投稿', href: '/posts/new')
+      end
+
+      it 'ログアウトリンクを表示する' do
+        expect(page).to have_content('ログアウト')
       end
     end
   end
