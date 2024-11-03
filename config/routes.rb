c104c920a7aec84e4565ce447b9d4249d07c51b3
koarakko5555 <hosayaan.1016@gmail.com>
2024-11-03 21:29:28 +0900

    学習ログ削除機能を作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#top'
 
-  resources :posts, only: %i[new create show index] # 追加
+  resources :posts, only: %i[new create show index destroy] # 追加
 end
