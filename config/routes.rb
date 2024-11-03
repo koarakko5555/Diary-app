3732315f0919a3d9886288bb6fab3b76a12f86f0
koarakko5555 <hosayaan.1016@gmail.com>
2024-11-03 21:12:44 +0900

    学習ログ一覧機能を作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#top'
 
-  resources :posts, only: %i[new create show] # 追加
+  resources :posts, only: %i[new create show index] # 追加
 end
